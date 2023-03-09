52c0fa5d2f1182284fd6ff1acd111a5c6d198c71
nuvme <pedrofmm13@gmail.com>
2023-03-09 14:01:38 -0300

    1009 soluction and input

---
+++ poo-1/1009-salario-bonus.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+nome = input()
+base = float(input())
+vendas = float(input())
+
+print(f"TOTAL = R$ {(base + (vendas * 0.15)):.2f}")
